b4e4ecdcbeac844ad690c13ae8485debd77e2f5a
rednoah <reinhard.pointner@gmail.com>
2015-05-03 20:59:23 +0800

    * support 256x256 icons (DSM 5) * don't perform cleanup immediately (to help users understand the spk file structure without having to unpack the resulting spk)

--- src/net/filebot/ant/spk/SpkTask.java
+++ src/net/filebot/ant/spk/SpkTask.java
@@ -42,7 +42,7 @@ public class SpkTask extends Task {
 
 			@Override
 			public String[] getValues() {
-				return new String[] { "72", "256" };
+				return new String[] { "72", "140", "256" };
 			}
 
 			public String getFileName() {
@@ -50,7 +50,7 @@ public class SpkTask extends Task {
 				case "72":
 					return "PACKAGE_ICON.PNG";
 				default:
-					return String.format("PACKAGE_ICON_%d.PNG", value);
+					return String.format("PACKAGE_ICON_%s.PNG", value);
 				}
 			}
 		}
